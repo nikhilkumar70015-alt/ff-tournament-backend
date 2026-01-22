05f842c28198254f9d646760c0cd366d0286d1ee
nikhilkumar70015-alt <nikhilkumar70015@gmail.com>
2026-01-22 22:07:29 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -248,7 +248,7 @@ router.get(
   async (req, res, next) => {
     try {
       const tournamentsCount = await Tournament.countDocuments();
-      const adminsCount = await User.countDocuments({role: "admin")};
+      const adminsCount = await User.countDocuments({ role: "admin" )};
       const usersCount = await User.countDocuments({ role: "user" });
 
       // Pending withdrawals = last transaction WITHDRAW_REQUEST
